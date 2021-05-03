70bd1cfb1d47a7e0db56da178fab859168a61874
DavidHart05 <>
2021-05-03 14:46:37 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// when the button is pressed function reportResults is used
+document.getElementById('results').addEventListener('click', reportResults)
+
+// alerts the input value
+function reportResults () {
+  alert(document.getElementById('input').value)
+}
